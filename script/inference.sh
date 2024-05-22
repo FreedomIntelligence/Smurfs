21e1faa503fb5d75a58069462a462ff54ae3fa5d
szjiozi <99324175+szjiozi@users.noreply.github.com>
2024-05-22 23:18:11 +0800

    Update inference.sh

--- script/inference.sh
+++ script/inference.sh
@@ -1,12 +1,12 @@
 export rapidapi_key=""
-export toolbench_key="lDPxqwOYRltCORiQt4viGwgAehsDt2pn5ez4W78hwK5nVtyPET"
+export toolbench_key=""
 export OPENAI_KEY=""
 
 model_name="Mistral-7B-Instruct-v0.2"
 method_name="smurfs"
-test_query_id_path="/mntcephfs/lab_data/chenjunzhi/data/test_query_ids"
-query_file_dir="/mntcephfs/lab_data/chenjunzhi/data/test_instruction"
-tool_env_dir="/mntcephfs/lab_data/chenjunzhi/data/toolenv/tools"
+test_query_id_path="toolbench_data/data/test_query_ids"
+query_file_dir="toolbench_data/data/test_instruction"
+tool_env_dir="toolbench_data/data/toolenv/tools"
 
 
 python Smurfs/inference/inference.py \
