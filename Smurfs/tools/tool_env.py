da1ab9c840e5807e909438a36f98e56cfbe9a962
陈君至 <szjiozi@163.com>
2024-06-27 14:20:42 +0800

    add code and math api

--- Smurfs/tools/tool_env.py
+++ Smurfs/tools/tool_env.py
@@ -1,7 +1,17 @@
+import warnings
+
+# 抑制所有警告
+warnings.filterwarnings('ignore')
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from Smurfs.tools.utils import python_repl, interpreter
 import requests
 from langchain import Wikipedia
 from langchain.docstore.base import Docstore
 from langchain.agents.react.base import DocstoreExplorer
+import xmltodict
+
 class HotpotToolEnv:
     def __init__(self):
         self.bingresults = None
@@ -33,7 +43,7 @@ class HotpotToolEnv:
             
         return responses
 
-    def call_bing_search(self, query, count, endpoint="https://api.bing.microsoft.com/v7.0/search", bing_api_key="ee2215eb571a49d4927328510c99fec6"):
+    def call_bing_search(self, query, count, endpoint="https://api.bing.microsoft.com/v7.0/search", bing_api_key=""):
         headers = {'Ocp-Apim-Subscription-Key': bing_api_key}
         params = {"q": query, "textDecorations": True,
                 "textFormat": "HTML", "count": count, "mkt": "en-GB"}
@@ -123,12 +133,139 @@ class HotpotToolEnv:
 
 HPEnv = HotpotToolEnv()
 
-tool_env = {
+
+#计算任务
+#经常不print，功能最强大
+def calculate_python(query):
+    query = query.strip().strip("```")
+    return python_repl.run(query)
+
+#做计算任务不错，但是不会做比较任务
+def code_interpreter(code):
+    '''execute Python expressions with Python Interpreter, can be used as a simple calculator e.g., "(123 + 234) / 23 * 19"
+        '''
+    return interpreter.execute_code(code) 
+
+def compare(candidate_list, compare_value, mode):
+    #candidate_list: {name: , value: }
+    results=[]
+    if mode == "gt":
+        for candidate in candidate_list:
+            if candidate["value"] >= compare_value:
+                results.append(candidate)
+        return results
+    if mode == "lt":
+        for candidate in candidate_list:
+            if candidate["value"] <= compare_value:
+                results.append(candidate)
+        return results
+    if mode == "eq":
+        for candidate in candidate_list:
+            if candidate["value"] == compare_value:
+                results.append(candidate)
+        return results
+    if mode == "max":
+        for candidate in candidate_list:
+            if results == []:
+                results.append(candidate)
+            else:
+                if candidate["value"] == results[0]["value"]:
+                    results.append(candidate)
+                elif candidate["value"] > results[0]["value"]:
+                    results = [candidate]
+        return results
+    if mode == "min":
+        for candidate in candidate_list:
+            if results == []:
+                results.append(candidate)
+            else:
+                if candidate["value"] == results[0]["value"]:
+                    results.append(candidate)
+                elif candidate["value"] < results[0]["value"]:
+                    results = [candidate]
+        return results
+    if mode == "nearest":
+        for candidate in candidate_list:
+            if results == []:
+                results.append(candidate)
+            else:
+                can_dff = abs(candidate["value"]-compare_value)
+                res_dff = abs(results[0]["value"]-compare_value)
+                if can_dff == res_dff:
+                    results.append(candidate)
+                elif can_dff < res_dff:
+                    results = [candidate]
+        return results
+    if mode == "farest":
+        for candidate in candidate_list:
+            if results == []:
+                results.append(candidate)
+            else:
+                can_dff = abs(candidate["value"]-compare_value)
+                res_dff = abs(results[0]["value"]-compare_value)
+                if can_dff == res_dff:
+                    results.append(candidate)
+                elif can_dff > res_dff:
+                    results = [candidate]
+        return results
+
+def getWolframAlphaResults(input:str, APPID = ""):
+        """Get Wolfram|Alpha results using natural query. Queries to getWolframAlphaResults must ALWAYS have this structure: {\"input\": query}. And please directly read the output json. 
+        """
+        URL = "https://api.wolframalpha.com/v2/query"
+
+        params = {'appid': APPID, "input": input, 'format': 'plaintext'}
+        
+        response = requests.get(URL, params=params)
+        
+        json_data = xmltodict.parse(response.text)
+        
+        if 'pod' not in json_data["queryresult"]:
+            return "WolframAlpha API cannot parse the input query."
+        #print(json_data)
+        rets = json_data["queryresult"]['pod']
+
+        cleaned_rets = []
+        blacklist = ["@scanner", "@id", "@position", "@error", "@numsubpods", "@width", "@height", "@type", "@themes","@colorinvertable", "expressiontypes"]
+        
+        def filter_dict(d, blacklist):
+            if isinstance(d, dict):
+                return {k: filter_dict(v, blacklist) for k, v in d.items() if k not in blacklist}
+            elif isinstance(d, list):
+                return [filter_dict(i, blacklist) for i in d]
+            else:
+                return d
+
+        for ret in rets:
+            ret = filter_dict(ret, blacklist=blacklist)
+            cleaned_rets.append(ret)
+            # print(ret)
+            # # Do further cleaning to retain only the input and result pods
+            # if "@title" in ret:
+            #     if ret["@title"] == "Input" or ret["@title"] == "Result":
+            #         cleaned_rets.append(ret)
+
+        return cleaned_rets
+
+# result = getWolframAlphaResults("what is the factorial of 3?")
+# print(result)
+
+tool_env_math = {
+    "calculate_python": calculate_python,
+    "code_interpreter": code_interpreter,
+    "getWolframAlphaResults": getWolframAlphaResults
+}
+
+tool_env_hotpot = {
     "BingSearch": HPEnv.BingSearch,
     "Retrieve": HPEnv.Retrieve,
     "Lookup": HPEnv.Lookup
     }
 
-# responses = HPEnv.BingSearch("Golden State Warriors")
-# responses = HPEnv.Lookup("Stephen Curry")
-# print(responses)
+# result = getWolframAlphaResults('Calc integral of sin(x)+2x^2+3x+1 from 0 to 1')
+# print(result)
+
+
+# # responses = HPEnv.BingSearch("Golden State Warriors")
+# # responses = HPEnv.Lookup("Stephen Curry")
+# # print(responses)
